6ebd1f76fa5b33ddfdf0775676f76a31a82404d1
ssskram <paul.marks@pittsburghpa.gov>
2019-02-14 12:33:04 -0500

    delete image point

--- routes/accmobile.js
+++ routes/accmobile.js
@@ -8,7 +8,7 @@ const getStream = require('into-stream')
 const containerName = 'accmobile'
 global.Headers = fetch.Headers
 
-// post image to accmobile container
+//post image to accmobile container
 router.post('/image', (req, res) => {
   const valid = (checkToken(req.token))
   if (valid == true) {
@@ -16,6 +16,21 @@ router.post('/image', (req, res) => {
     const stream = getStream(req.files.file.data)
     const streamLength = req.files.file.data.length
     blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, error => {
+      if (!error) {
+        res.status(200).send()
+      } else res.status(500).send(error)
+    })
+  } else res.status(403).end()
+})
+
+// remove image from container
+router.post('/deleteImage', (req, res) => {
+  const valid = (checkToken(req.token))
+  if (valid == true) {
+    const blobName = req.query.filename
+    const stream = getStream(req.files.file.data)
+    const streamLength = req.files.file.data.length
+    blobService.deleteBlobfromStream(containerName, blobName, stream, streamLength, error => {
       if (!error) {
         res.status(200).send()
       } else res.status(500).send(error)
