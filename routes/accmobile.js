e1ed238bf37165f5839ccbffa23c7bbc19768f7d
ssskram <ssskram@ssskram.local>
2019-03-25 16:13:00 -0400

    arrows

--- routes/accmobile.js
+++ routes/accmobile.js
@@ -1,39 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const fetch = require('node-fetch')
-const checkToken = require('../token')
 const azureStorage = require('azure-storage')
 const blobService = azureStorage.createBlobService()
 const getStream = require('into-stream')
 const containerName = 'accmobile'
-global.Headers = fetch.Headers
 
 // post image to accmobile container
 router.post('/image', (req, res) => {
-  const valid = (checkToken(req.token))
-  if (valid == true) {
-    const blobName = req.query.filename
-    const stream = getStream(req.files.file.data)
-    const streamLength = req.files.file.data.length
-    blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, error => {
-      if (!error) {
-        res.status(200).send()
-      } else res.status(500).send(error)
-    })
-  } else res.status(403).end()
+  const blobName = req.query.filename
+  const stream = getStream(req.files.file.data)
+  const streamLength = req.files.file.data.length
+  blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, error => {
+    if (!error) {
+      res.status(200).send()
+    } else res.status(500).send(error)
+  })
 })
 
 // remove image from container
 router.get('/deleteImage', (req, res) => {
-  const valid = (checkToken(req.token))
-  if (valid == true) {
-    const blobName = req.query.blobName
-    blobService.deleteBlob(containerName, blobName, {}, error => {
-      if (!error) {
-        res.status(200).send()
-      } else res.status(500).send(error)
-    })
-  } else res.status(403).end()
+  const blobName = req.query.blobName
+  blobService.deleteBlob(containerName, blobName, {}, error => {
+    if (!error) {
+      res.status(200).send()
+    } else res.status(500).send(error)
+  })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bearerToken = require('express-bearer-token')
 const cors = require('cors')
-const fileUpload = require('express-fileupload')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+const checkToken = require('./token')
+const fetch = require("node-fetch")
+global.Headers = fetch.Headers
 
 // import env variables
 require('dotenv').config()
@@ -11,18 +15,8 @@ require('dotenv').config()
 var app = express()
 app.set('port', process.env.PORT || 3000)
 
-// docs
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-// bearer token
-app.use(bearerToken())
-
-// parsin
+// body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
-app.use(fileUpload())
 
 // enable cors on all requests
 app.use(cors())
@@ -30,17 +24,34 @@ app.use(cors())
 // logging
 app.use(require('morgan')('combined'))
 
+// docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// bearer token
+app.use(bearerToken())
+
+// access control
+app.use((req, res, next) => {
+  const valid = (checkToken(req.token))
+  if (valid == true) {
+    next()
+  } else {
+    res.status(403).end()
+  }
+})
+
+
 // routes
 app.use("/accMobile", require('./routes/accmobile'))
 
 // Production error handler
 if (app.get('env') === 'production') {
-  app.use(function (err, req, res, next) {
+  app.use((err, req, res, next) => {
     console.error(err.stack)
     res.sendStatus(err.status || 500)
   })
 }
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), () => {
   console.log('Express server listening on port ' + app.get('port'))
 })
