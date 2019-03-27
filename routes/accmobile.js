ffc3391eeab3ba62456dcd44432b7c92f35314cc
ssskram <ssskram@ssskram.local>
2019-03-27 12:34:02 -0400

    prettify

--- routes/accmobile.js
+++ routes/accmobile.js
@@ -1,30 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const azureStorage = require('azure-storage')
-const blobService = azureStorage.createBlobService()
-const getStream = require('into-stream')
-const containerName = 'accmobile'
+const express = require("express");
+const router = express.Router();
+const azureStorage = require("azure-storage");
+const blobService = azureStorage.createBlobService();
+const getStream = require("into-stream");
+const containerName = "accmobile";
 
 // post image to accmobile container
-router.post('/image', (req, res) => {
-  const blobName = req.query.filename
-  const stream = getStream(req.files.file.data)
-  const streamLength = req.files.file.data.length
-  blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, error => {
-    if (!error) {
-      res.status(200).send()
-    } else res.status(500).send(error)
-  })
-})
+router.post("/image", (req, res) => {
+  const blobName = req.query.filename;
+  const stream = getStream(req.files.file.data);
+  const streamLength = req.files.file.data.length;
+  blobService.createBlockBlobFromStream(
+    containerName,
+    blobName,
+    stream,
+    streamLength,
+    error => {
+      if (!error) {
+        res.status(200).send();
+      } else res.status(500).send(error);
+    }
+  );
+});
 
 // remove image from container
-router.get('/deleteImage', (req, res) => {
-  const blobName = req.query.blobName
+router.get("/deleteImage", (req, res) => {
+  const blobName = req.query.blobName;
   blobService.deleteBlob(containerName, blobName, {}, error => {
     if (!error) {
-      res.status(200).send()
-    } else res.status(500).send(error)
-  })
-})
+      res.status(200).send();
+    } else res.status(500).send(error);
+  });
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,57 +1,56 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const bearerToken = require('express-bearer-token')
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
-const checkToken = require('./token')
-const fetch = require("node-fetch")
-global.Headers = fetch.Headers
+const express = require("express");
+const bodyParser = require("body-parser");
+const bearerToken = require("express-bearer-token");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const checkToken = require("./token");
+const fetch = require("node-fetch");
+global.Headers = fetch.Headers;
 
 // import env variables
-require('dotenv').config()
+require("dotenv").config();
 
 // start express
-var app = express()
-app.set('port', process.env.PORT || 3000)
+var app = express();
+app.set("port", process.env.PORT || 3000);
 
 // body parser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // enable cors on all requests
-app.use(cors())
+app.use(cors());
 
 // logging
-app.use(require('morgan')('combined'))
+app.use(require("morgan")("combined"));
 
 // docs
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // bearer token
-app.use(bearerToken())
+app.use(bearerToken());
 
 // access control
 app.use((req, res, next) => {
-  const valid = (checkToken(req.token))
+  const valid = checkToken(req.token);
   if (valid == true) {
-    next()
+    next();
   } else {
-    res.status(403).end()
+    res.status(403).end();
   }
-})
-
+});
 
 // routes
-app.use("/accMobile", require('./routes/accmobile'))
+app.use("/accMobile", require("./routes/accmobile"));
 
 // Production error handler
-if (app.get('env') === 'production') {
+if (app.get("env") === "production") {
   app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.sendStatus(err.status || 500)
-  })
+    console.error(err.stack);
+    res.sendStatus(err.status || 500);
+  });
 }
 
-app.listen(app.get('port'), () => {
-  console.log('Express server listening on port ' + app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
+});
